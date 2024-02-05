9160bf6fc2efbb4ce7d25faa4f56cb59529651d7
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-02-05 19:38:25 +0300

    add more pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from '../src/components/Header/Header.jsx'
 import './App.css';
-import MainPage from '../src/components/Mainpage/MainPage.jsx';
+// import MainPage from '../src/components/Mainpage/MainPage.jsx';
+import FruitPage from '../src/components/Fruitpage/FruitPage.jsx';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <MainPage></MainPage>
+      {/* <MainPage></MainPage> */}
+      <FruitPage></FruitPage>
     </>
   );
 }
